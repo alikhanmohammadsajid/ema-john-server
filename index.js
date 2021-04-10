3cd8a7badb51eb7927a223cdf2925ab8dd2e0e07
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-04-11 00:34:30 +0600

    payment updated

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ client.connect(err => {
       })
   })
   app.get('/products', (req, res) => {
-    productsCollection.find({})
+    const search = req.query.search
+    productsCollection.find({name: {$regex: search} })
       .toArray((err, documents) => {
         res.send(documents);
       })
@@ -64,8 +65,6 @@ client.connect(err => {
   })
 
 });
-
-
 
 
 app.listen(process.env.PORT || port)
